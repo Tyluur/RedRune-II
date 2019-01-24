ad24d0cd40960561627eebbe9af8f164cf3884fa
Tyluur <itstyluur@gmail.com>
2019-01-24 18:30:11 -0500

    Client refactoring, lobby auto transmission into game
    Signed-off-by: Tyluur <Tyluur@users.noreply.github.com>

--- src/com/rs/networking/codec/encode/LoginPacketsEncoder.java
+++ src/com/rs/networking/codec/encode/LoginPacketsEncoder.java
@@ -73,7 +73,6 @@ public final class LoginPacketsEncoder extends Encoder {
 		responseBlock.writeShort(0);
 		// leave the cast because of json boxing.
 		long lastLogin = System.currentTimeMillis(); /*((Number) player.getVariables().getAttribute(AttributeKey.LAST_LONGIN_STAMP, System.currentTimeMillis())).longValue();*/
-		
 		long now = System.currentTimeMillis();
 		long jag = 1014753880308L;
 		long since_jag = (now - jag) / 1000 / 60 / 60 / 24;
@@ -84,69 +83,18 @@ public final class LoginPacketsEncoder extends Encoder {
 		if (lastIp == null) {
 			lastIp = Misc.getIpAddress(player.getSession().getChannel());
 		}
-		responseBlock.writeInt(Misc.IPAddressToNumber(lastIp)); // last login ip
-		
+		responseBlock.writeInt(Misc.IPAddressToNumber(lastIp));
 		responseBlock.writeByte(3);
 		responseBlock.writeShort(0);
-		responseBlock.writeShort(0);
-		responseBlock.writeByte(1);
+		responseBlock.writeShort(0); // 		loginResponse.AppendShort(0);
+		responseBlock.writeByte(0); // unused in client
 		responseBlock.writeGJString(player.getUsername());
-		responseBlock.writeByte((byte) 0);
-		responseBlock.writeInt(1);
-		responseBlock.writeByte(1);
-		responseBlock.writeShort(1);
+		responseBlock.writeByte(0); // // unused in client
+		responseBlock.writeInt(0); //  loginResponse.AppendInt(character.Name.StartsWith("#") ? 0 : 1);
+		responseBlock.writeByte(0); // // dunno (bool)
+		responseBlock.writeShort(1); // // worldid
 		responseBlock.writeGJString("127.0.0.1");
 		responseBlock.endPacketVarByte();
-		
-		/*
-		builder.writeByte(player.getDetails().getDominantRight().getClientRight());
-		builder.writeByte(0); // blackmarks
-		builder.writeByte(0); // muted? (bool)
-		builder.writeByte(0); // dunno (bool)
-		builder.writeByte(0);// dunno (bool)
-		final long nowDate = new Date().getTime();
-		
-		builder.writeLong((nowDate - new Date(1970, 1, 1).getTime()));
-		builder.writeByte5((6 * (60000)) + 20000);
-		builder.writeByte(player.getDetails().isDonator() ? 0x2 : 0);
-		builder.writeInt(0);// unused in client
-		builder.writeByte(0); // unused in client (bool)
-		builder.writeInt(0); // unused in client
-		builder.writeShort(1); // recovery questions, 0 - not set,
-		// otherwise goes bitencoded date.
-		builder.writeShort(player.getManager().getWebManager().getUnreadMessages()); // unread messages
-		
-		// leave the cast because of json boxing.
-		long lastLogin = ((Number) player.getVariables().getAttribute(AttributeKey.LAST_LONGIN_STAMP, System.currentTimeMillis())).longValue();
-		
-		long now = System.currentTimeMillis();
-		long jag = 1014753880308L;
-		long since_jag = (now - jag) / 1000 / 60 / 60 / 24;
-		long since_log = (now - lastLogin) / 1000 / 60 / 60 / 24;
-		
-		builder.writeShort((int) (since_jag - since_log)); // last logged in date
-		String lastIp = player.getDetails().getLastIp();
-		if (lastIp == null) {
-			lastIp = Misc.getIpAddress(player.getSession().getChannel());
-		}
-		builder.writeInt(Misc.IPAddressToNumber(lastIp)); // last login ip
-		builder.writeByte(player.getManager().getWebManager().isEmailVerified() ? 3 : 0);
-		// 0 - no email
-		// 1 - pending parental confirmation
-		// 2 - pending confirmation.
-		// 3 - registered.
-		// 4 - no longer registered.
-		builder.writeShort(0); // credit card expiration time
-		builder.writeShort(0); // credit card loyality expiration time
-		builder.writeByte(0); // unused in client
-		builder.writeGJString2(player.getDetails().getUsername());
-		builder.writeByte(0); // unused in client
-		builder.writeInt(player.getDetails().getUsername().startsWith("#") ? 0 : 1);
-		builder.writeByte(1); // dunno (bool)
-		builder.writeShort(GameFlags.worldId); // current world id
-		builder.writeGJString2("127.0.0.1");
-		 */
-		
 		player.getSession().write(responseBlock);
 	}
 
