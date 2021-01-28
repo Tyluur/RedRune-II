4474bc38ba5447636b1c66a2d0a30a4140c69fc7
Tyluur <itstyluur@icloud.com>
2021-01-28 14:14:27 -0500

    list players online in the ::players command

--- src/main/java/plugin/command/player/PlayersOnlineCommandPlugin.java
+++ src/main/java/plugin/command/player/PlayersOnlineCommandPlugin.java
@@ -1,9 +1,13 @@
 package plugin.command.player;
 
-import org.redrune.game.entity.actor.player.Player;
 import org.redrune.game.content.plugin.type.CommandPlugin;
+import org.redrune.game.entity.actor.player.Player;
 import org.redrune.game.global.World;
+import org.redrune.utility.constants.InterfaceConstants;
 import plugin.command.CommandManifest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Tyluur <itstyluur@icloud.com>
@@ -11,14 +15,21 @@ import plugin.command.CommandManifest;
  */
 @CommandManifest(description = "Shows the players online")
 public class PlayersOnlineCommandPlugin extends CommandPlugin {
-	
-	@Override
-	public void handle(Player player, String[] args, boolean console, boolean clientCommand) {
-		sendResponse(player, "There are currently " + World.getPlayers().size() + " players online.", console);
-	}
-	
-	@Override
-	public String[] identifiers() {
-		return arguments("players");
-	}
+
+    @Override
+    public void handle(Player player, String[] args, boolean console, boolean clientCommand) {
+        List<String> messages = new ArrayList<>();
+
+        World.playerStream().forEach(p -> {
+            messages.add("" + p.getDisplayName() + " (lvl. " + p.getSkills().getCombatLevel() + ")");
+        });
+
+        player.getPackets().sendMessage("There are currently " + World.getPlayers().size() + " players online.", console);
+        InterfaceConstants.sendQuestScroll(player, "Dusk", messages.toArray(new String[messages.size()]));
+    }
+
+    @Override
+    public String[] identifiers() {
+        return arguments("players");
+    }
 }
