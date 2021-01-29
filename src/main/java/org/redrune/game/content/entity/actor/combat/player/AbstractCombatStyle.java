18737c0bd4003ea05774c5b68e59a53e00792673
Tyluur <itstyluur@icloud.com>
2021-01-28 19:50:24 -0500

    remove untested combat changes

--- src/main/java/org/redrune/game/content/entity/actor/combat/player/AbstractCombatStyle.java
+++ src/main/java/org/redrune/game/content/entity/actor/combat/player/AbstractCombatStyle.java
@@ -13,70 +13,69 @@ import org.redrune.utility.constants.SkillConstants;
  * @since 9/4/2017
  */
 public abstract class AbstractCombatStyle implements SkillConstants {
-	
-	/**
-	 * This method handles the swing of a combat style
-	 */
-	public abstract boolean fireSwing(Player source, Actor target);
-	
-	/**
-	 * Handles the addition of experience
-	 */
-	public abstract void addExperience(Player source, Actor target, Hit hit, int attackStyle, int weaponId);
-	
-	/**
-	 * Gets the random damage, based on the calculated maximum values
-	 *
-	 * @param source
-	 * 		The source
-	 */
-	public abstract int getRandomDamage(Actor source, Actor target, double multiplier);
-	
-	/**
-	 * Sends the hit to the target
-	 */
-	public abstract CombatSwingDetail sendHit(Player source, Actor target, int maxHit, int damage, int delay);
-	
-	/**
-	 * The combat style calculator
-	 */
-	protected final AbstractCombatCalculator calculator;
-	
-	/**
-	 * Constructs a new combat style enumeration instance
-	 */
-	protected AbstractCombatStyle(AbstractCombatCalculator calculator) {
-		this.calculator = calculator;
-	}
-	
-	/**
-	 * Plays a sound to a single player
-	 */
-	public void playSingleSound(Player player, int soundId) {
-		if (soundId == -1) {
-			return;
-		}
-		player.getPackets().sendSound(soundId, 0, 1);
-	}
-	
-	/**
-	 * Plays a sound to all players in the source's area, within a 5 tile radius
-	 */
-	public void playAreaSound(Player player, int soundId) {
-		if (soundId == -1) {
-			return;
-		}
-		Region region = RegionManager.getRegion(player.getRegionId());
-		if (region == null) {
-			return;
-		}
-		for (Player p : region.getPlayersWithinDistance(player, 5)) {
-			if (p == null) {
-				continue;
-			}
-			p.getPackets().sendSound(soundId, 0, 1);
-		}
-	}
+
+    /**
+     * This method handles the swing of a combat style
+     */
+    public abstract boolean fireSwing(Player source, Actor target);
+
+    /**
+     * Handles the addition of experience
+     */
+    public abstract void addExperience(Player source, Actor target, Hit hit, int attackStyle, int weaponId);
+
+    /**
+     * Gets the random damage, based on the calculated maximum values
+     *
+     * @param source The source
+     */
+    public abstract int getRandomDamage(Actor source, Actor target, double multiplier);
+
+    /**
+     * Sends the hit to the target
+     */
+    public abstract CombatSwingDetail sendHit(Player source, Actor target, int maxHit, int damage, int delay);
+
+    /**
+     * The combat style calculator
+     */
+    protected final AbstractCombatCalculator calculator;
+
+    /**
+     * Constructs a new combat style enumeration instance
+     */
+    protected AbstractCombatStyle(AbstractCombatCalculator calculator) {
+        this.calculator = calculator;
+    }
+
+    /**
+     * Plays a sound to a single player
+     */
+    public void playSingleSound(Player player, int soundId) {
+        if (soundId == -1) {
+            return;
+        }
+        player.getPackets().sendSound(soundId, 0, 1);
+    }
+
+    /**
+     * Plays a sound to all players in the source's area, within a 5 tile radius
+     */
+    public void playAreaSound(Player player, int soundId) {
+        if (soundId == -1) {
+            return;
+        }
+        Region region = RegionManager.getRegion(player.getRegionId());
+        if (region == null) {
+            return;
+        }
+        for (Player p : region.getPlayersWithinDistance(player, 5)) {
+            if (p == null) {
+                continue;
+            }
+            p.getPackets().sendSound(soundId, 0, 1);
+        }
+    }
 
     public AbstractCombatCalculator getCalculator() {
         return this.calculator;
