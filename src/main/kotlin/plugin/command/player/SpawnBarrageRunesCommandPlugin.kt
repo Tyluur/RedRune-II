121f0894c7a337f06b54c74dfd4dadc136d33c4d
Tyluur <itstyluur@icloud.com>
2021-01-28 21:59:47 -0500

    add veng and barrage commands

---
+++ src/main/kotlin/plugin/command/player/SpawnBarrageRunesCommandPlugin.kt
@@ -0,0 +1,22 @@
+package plugin.command.player
+
+import org.redrune.game.content.plugin.type.CommandPlugin
+import org.redrune.game.entity.actor.player.Player
+
+/**
+ * @author Tyluur <itstyluur@icloud.com>
+ * @since January 28, 2021
+ */
+class SpawnBarrageRunesCommandPlugin : CommandPlugin() {
+
+    override fun handle(player: Player, args: Array<out String>, console: Boolean, clientCommand: Boolean) {
+        val inventory = player.inventory
+
+        inventory.addItem(565, 5000)
+        inventory.addItem(560, 5000)
+        inventory.addItem(555, 5000)
+    }
+
+    override fun identifiers() = arrayOf("barrage")
+
+}
---
+++ src/main/kotlin/plugin/command/player/SpawnVengRunesCommandPlugin.kt
@@ -0,0 +1,20 @@
+package plugin.command.player
+
+import org.redrune.game.content.plugin.type.CommandPlugin
+import org.redrune.game.entity.actor.player.Player
+
+/**
+ * @author Tyluur <itstyluur@icloud.com>
+ * @since January 28, 2021
+ */
+class SpawnVengRunesCommandPlugin : CommandPlugin() {
+    override fun handle(player: Player, args: Array<out String>, console: Boolean, clientCommand: Boolean) {
+        val inventory = player.inventory
+
+        inventory.addItem(9075, 5000)
+        inventory.addItem(557, 5000)
+        inventory.addItem(560, 5000)
+    }
+
+    override fun identifiers() = arguments("veng")
+}
