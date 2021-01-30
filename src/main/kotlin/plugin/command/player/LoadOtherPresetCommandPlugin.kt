32ed11b7d39aea482123bfb219068b76abe68b67
raynna <53792175+raynna@users.noreply.github.com>
2021-01-30 03:25:38 +0100

    LoadOtherPresetCommandPlugin.kt

---
+++ src/main/kotlin/plugin/command/player/LoadOtherPresetCommandPlugin.kt
@@ -0,0 +1,30 @@
+package plugin.command.player
+
+import org.redrune.game.content.plugin.type.CommandPlugin
+import org.redrune.game.entity.actor.player.Player
+import org.redrune.game.global.World
+import org.redrune.utility.game.InputEvent
+import plugin.command.CommandManifest
+
+@CommandManifest(description = "Loads a preset of otherplayers", types = [String::class])
+class LoadOtherPresetCommandPlugin : CommandPlugin() {
+    override fun handle(player: Player, args: Array<String>, console: Boolean, clientCommand: Boolean) {
+        val name = getCompleted(args, 1)
+        player.packets.requestClientInput(object :
+            InputEvent("Enter players name:", InputEventType.LONG_TEXT) {
+            override fun handleInput() {
+                val target = World.getPlayerByDisplayName(name) ?: return
+                player.packets.requestClientInput(object :
+                    InputEvent("Enter other players presetname:", InputEventType.LONG_TEXT) {
+                    override fun handleInput() {
+                        player.presetManager.loadPreset(name, target);
+                    }
+                })
+            }
+        })
+    }
+
+    override fun identifiers(): Array<String> {
+        return arguments("loadother")
+    }
+}
