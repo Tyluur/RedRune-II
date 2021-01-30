e15c7d517accd55e935e577b03fb5c840347db12
raynna <53792175+raynna@users.noreply.github.com>
2021-01-30 03:27:00 +0100

    SavePresetCommandPlugin.kt

--- src/main/kotlin/plugin/command/player/SavePresetCommandPlugin.kt
+++ src/main/kotlin/plugin/command/player/SavePresetCommandPlugin.kt
@@ -10,56 +10,21 @@ import org.redrune.game.entity.actor.player.data.PlayerRight
 import org.redrune.utility.constants.InterfaceConstants
 import org.redrune.cache.loaders.ItemDefinitions
 import org.redrune.game.entity.actor.player.Player
+import org.redrune.utility.game.InputEvent
 
-/**
- * @author Tyluur <itstyluur></itstyluur>@icloud.com>
- * @since 9/1/2017
- */
-@CommandManifest(description = "Copies another player", types = [String::class])
+@CommandManifest(description = "Saves a new preset", types = [String::class])
 class SavePresetCommandPlugin : CommandPlugin() {
     override fun handle(player: Player, args: Array<String>, console: Boolean, clientCommand: Boolean) {
-        val username = getCompleted(args, 1)
-        val p2 = World.getPlayerByDisplayName(username)
-        if (p2 == null) {
-            player.packets.sendMessage("Couldn't find player $username.")
-            return
-        }
-        val items = p2.equipment.items.itemsCopy
-        for (i in items.indices) {
-            if (items[i] == null) {
-                continue
+        val name = getCompleted(args, 1)
+        player.packets.requestClientInput(object :
+            InputEvent("Enter name of new preset:", InputEventType.LONG_TEXT) {
+            override fun handleInput() {
+                player.presetManager.savePreset(name);
             }
-            val skillRequirements = items[i]!!.definitions.wearingSkillRequirements
-            var hasRequirements = true
-            if (skillRequirements != null) {
-                for (skillId in skillRequirements.keys) {
-                    if (skillId > 24 || skillId < 0) {
-                        continue
-                    }
-                    val level = skillRequirements[skillId]!!
-                    if (level < 0 || level > 120) {
-                        continue
-                    }
-                    if (player.skills.getLevelForXp(skillId) < level) {
-                        if (hasRequirements) {
-                            player.packets.sendMessage("You are not high enough level to use this item.")
-                        }
-                        hasRequirements = false
-                        val name = SkillConstants.SKILL_NAME[skillId].toLowerCase()
-                        player.packets.sendMessage("You need to have a" + (if (name.startsWith("a")) "n" else "") + " " + name + " level of " + level + ".")
-                    }
-                }
-            }
-            if (!hasRequirements) {
-                return
-            }
-            player.equipment.items[i] = items[i]
-            player.equipment.refresh(i)
-        }
-        player.appearance.generateAppearanceData()
+        })
     }
 
     override fun identifiers(): Array<String> {
-        return arguments("copy")
+        return arguments("save")
     }
 }
