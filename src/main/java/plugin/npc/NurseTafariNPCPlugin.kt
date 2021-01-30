f5eee7bfb4af0c7b8a8fed92a27800c961ba2625
Tyluur <itstyluur@icloud.com>
2021-01-29 20:55:48 -0500

    add nurse tafari

---
+++ src/main/java/plugin/npc/NurseTafariNPCPlugin.kt
@@ -0,0 +1,50 @@
+package plugin.npc
+
+import org.redrune.game.content.entity.actor.player.dialogue.impl.SimpleNPCMessage
+import org.redrune.game.content.plugin.type.NPCPlugin
+import org.redrune.game.entity.actor.mask.Animation
+import org.redrune.game.entity.actor.mask.Graphics
+import org.redrune.game.entity.actor.npc.NPC
+import org.redrune.game.entity.actor.player.Player
+
+/**
+ * @author Tyluur <itstyluur@icloud.com>
+ * @since January 29, 2021
+ */
+class NurseTafariNPCPlugin : NPCPlugin {
+
+    override fun register() {
+        registerNPC(961, "Talk-to")
+        registerNPC(961, "Heal")
+    }
+
+    override fun handle(player: Player, npc: NPC, option: String): Boolean {
+        healPlayer(player, npc)
+        return true
+    }
+
+    /**
+     * Heals the player
+     *
+     * @param player
+     * The player
+     * @param npc
+     * The npc
+     */
+    fun healPlayer(player: Player, npc: NPC): Boolean {
+        /** Performing a cool npc interaction  */
+        npc.nextAnimation = Animation(12575)
+        player.setNextGraphics(Graphics(1314))
+        /** Refreshing all player characteristics to optimal settings  */
+        player.restoreAll()
+        /** Sending surgeon dialogue  */
+        player.dialogueManager.startDialogue(
+            SimpleNPCMessage::class.java,
+            961,
+            "I have restored your character to extreme health!"
+        )
+        player.packets.sendMessage("You feel refreshed, past your normal health levels.")
+        return false
+    }
+
+}
