c1e6f12de99471250ee1ab81ee6b919161db4994
Tyluur <itstyluur@icloud.com>
2021-02-06 14:02:44 -0500

    convert all object plugins to kotlin

--- src/main/kotlin/plugin/object/NexEntranceObjectPlugin.kt
+++ src/main/kotlin/plugin/object/NexEntranceObjectPlugin.kt
@@ -1,27 +1,25 @@
-package plugin.object;
+package plugin.`object`
 
-import org.redrune.game.entity.actor.player.Player;
-import org.redrune.game.entity.object.WorldObject;
-import org.redrune.game.content.plugin.type.ObjectPlugin;
+import org.redrune.game.content.plugin.type.ObjectPlugin
+import org.redrune.game.entity.`object`.WorldObject
+import org.redrune.game.entity.actor.player.Player
 
 /**
  * @author Tyluur <itstyluur@icloud.com>
  * @since 9/1/2017
  */
-public class NexEntranceObjectPlugin implements ObjectPlugin {
-	
-	@Override
-	public boolean handle(Player player, WorldObject object, String option) {
-		switch (option) {
-			case "Climb-over":
-				player.getDialogueManager().startDialogue("NexEntrance");
-				return true;
-		}
-		return false;
-	}
-	
-	@Override
-	public void register() {
-		registerObject(57225, "Climb-over");
-	}
+class NexEntranceObjectPlugin : ObjectPlugin {
+    override fun handle(player: Player, `object`: WorldObject, option: String): Boolean {
+        when (option) {
+            "Climb-over" -> {
+                player.dialogueManager.startDialogue("NexEntrance")
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun register() {
+        registerObject(57225, "Climb-over")
+    }
 }
--- src/main/kotlin/plugin/object/agility/AgilityShortcutsPlugin.kt
+++ src/main/kotlin/plugin/object/agility/AgilityShortcutsPlugin.kt
@@ -1,29 +1,25 @@
-package plugin.object.agility;
+package plugin.`object`.agility
 
-import org.redrune.game.content.entity.actor.player.skills.agility.Shortcuts;
-import org.redrune.game.content.plugin.type.ObjectPlugin;
-import org.redrune.game.entity.actor.player.Player;
-import org.redrune.game.entity.object.WorldObject;
-
-import static org.redrune.utility.game.ClickOption.FIRST;
+import org.redrune.game.content.entity.actor.player.skills.agility.Shortcuts
+import org.redrune.game.content.plugin.type.ObjectPlugin
+import org.redrune.game.entity.`object`.WorldObject
+import org.redrune.game.entity.actor.player.Player
+import org.redrune.utility.game.ClickOption
 
 /**
  * @author Tyluur <itstyluur@icloud.com>
  * @since 2019-01-29
  */
-public class AgilityShortcutsPlugin implements ObjectPlugin {
-	
-	@Override
-	public boolean handle(Player player, WorldObject object, String option) {
-		int id = object.getId();
-		if (id == 9311 || id == 9312) {
-			Shortcuts.handleEdgevilleUnderwallTunnel(player, object);
-		}
-		return true;
-	}
-	
-	@Override
-	public void register() {
-		registerSpecifiedOptionVarags(FIRST, 9311, 9312);
-	}
+class AgilityShortcutsPlugin : ObjectPlugin {
+    override fun handle(player: Player, `object`: WorldObject, option: String): Boolean {
+        val id: Int = `object`.getId()
+        if (id == 9311 || id == 9312) {
+            Shortcuts.handleEdgevilleUnderwallTunnel(player, `object`)
+        }
+        return true
+    }
+
+    override fun register() {
+        registerSpecifiedOptionVarags(ClickOption.FIRST, 9311, 9312)
+    }
 }
