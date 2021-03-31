20c4e98c41b702604ec7e98798f0a40627e62528
Tyluur <itstyluur@icloud.com>
2021-03-31 19:20:31 -0400

    remove incoming packet queuing

--- src/main/java/org/redrune/networking/packet/incoming/IncomingPacketRepository.java
+++ src/main/java/org/redrune/networking/packet/incoming/IncomingPacketRepository.java
@@ -14,62 +14,59 @@ import java.util.concurrent.ConcurrentHashMap;
  * @since 2019-02-04
  */
 public class IncomingPacketRepository {
-	
-	/**
-	 * The map of {@code IncomingPacketDecoder}s
-	 */
-	private static final ConcurrentHashMap<Integer, IncomingPacketReader> PACKET_MAP = new ConcurrentHashMap<>();
-	
-	/**
-	 * Initializes all decoders
-	 */
-	public static void initialize() {
-		Misc.getClasses(IncomingPacketRepository.class.getPackage().getName() + ".impl").stream().filter(IncomingPacketReader.class::isInstance).forEach((clazz) -> include((IncomingPacketReader) clazz));
-		System.out.println("Initialized " + PACKET_MAP.size() + " incoming packet decoders");
-	}
-	
-	/**
-	 * Includes the decoder
-	 *
-	 * @param decoder
-	 * 		The decoder instance
-	 */
-	private static void include(IncomingPacketReader decoder) {
-		Arrays.stream(decoder.bindings()).forEach(key -> {
-			if (PACKET_MAP.containsKey(key)) {
-				throw new IllegalStateException("Defined incoming packet [" + PACKET_MAP.get(key) + "] #" + key + " already and attempted to store " + decoder + " ahead of it.");
-			}
-			PACKET_MAP.put(key, decoder);
-		});
-	}
-	
-	/**
-	 * Handling the decoding of the packet received
-	 *
-	 * @param player
-	 * 		The player whose session received the packet
-	 * @param packet
-	 * 		The packet that will be read
-	 */
-	public static void handlePacket(Player player, Packet packet) {
-		try {
-			int packetId = packet.getOpcode();
-			IncomingPacketReader reader = PACKET_MAP.get(packetId);
-			if (reader == null) {
-				System.out.println("Received game packet #" + packetId + ", unidentified handler.");
-				return;
-			}
-			PacketContext context = reader.read(player, packet);
-			if (context == null) {
-				if (GameFlags.debugMode) {
-					System.out.println("Unable to get packet context for incoming packet " + packet + " with [reader=" + reader.getClass().getSimpleName() + "]!");
-				}
-				return;
-			}
-			player.getSession().addContext(context);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+
+    /**
+     * The map of {@code IncomingPacketDecoder}s
+     */
+    private static final ConcurrentHashMap<Integer, IncomingPacketReader> PACKET_MAP = new ConcurrentHashMap<>();
+
+    /**
+     * Initializes all decoders
+     */
+    public static void initialize() {
+        Misc.getClasses(IncomingPacketRepository.class.getPackage().getName() + ".impl").stream().filter(IncomingPacketReader.class::isInstance).forEach((clazz) -> include((IncomingPacketReader) clazz));
+        System.out.println("Initialized " + PACKET_MAP.size() + " incoming packet decoders");
+    }
+
+    /**
+     * Includes the decoder
+     *
+     * @param decoder The decoder instance
+     */
+    private static void include(IncomingPacketReader decoder) {
+        Arrays.stream(decoder.bindings()).forEach(key -> {
+            if (PACKET_MAP.containsKey(key)) {
+                throw new IllegalStateException("Defined incoming packet [" + PACKET_MAP.get(key) + "] #" + key + " already and attempted to store " + decoder + " ahead of it.");
+            }
+            PACKET_MAP.put(key, decoder);
+        });
+    }
+
+    /**
+     * Handling the decoding of the packet received
+     *
+     * @param player The player whose session received the packet
+     * @param packet The packet that will be read
+     */
+    public static void handlePacket(Player player, Packet packet) {
+        try {
+            int packetId = packet.getOpcode();
+            IncomingPacketReader reader = PACKET_MAP.get(packetId);
+            if (reader == null) {
+                System.out.println("Received game packet #" + packetId + ", unidentified handler.");
+                return;
+            }
+            PacketContext context = reader.read(player, packet);
+            if (context == null) {
+                if (GameFlags.debugMode) {
+                    System.out.println("Unable to get packet context for incoming packet " + packet + " with [reader=" + reader.getClass().getSimpleName() + "]!");
+                }
+                return;
+            }
+            context.handle(player);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
