c051c0dfcf6832c6a97838c7d7387a799d9054f2
Tyluur <itstyluur@icloud.com>
2021-04-02 10:17:29 -0400

    convert npc extension holder to kotlin

--- src/main/java/org/redrune/game/entity/actor/npc/data/extension/NPCExtensionHolder.kt
+++ src/main/java/org/redrune/game/entity/actor/npc/data/extension/NPCExtensionHolder.kt
@@ -1,44 +1,43 @@
-package org.redrune.game.entity.actor.npc.data.extension;
+package org.redrune.game.entity.actor.npc.data.extension
 
-import org.redrune.game.entity.actor.npc.data.extension.impl.TalkingNPCExtension;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
+import com.github.michaelbull.logging.InlineLogger
+import org.redrune.game.entity.actor.npc.data.extension.impl.TalkingNPCExtension
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
- * @author Tyluur <itstyluur@icloud.com>
+ * @author Tyluur <itstyluur></itstyluur>@icloud.com>
  * @since 9/13/2017
  */
-public class NPCExtensionHolder {
-	
-	/**
-	 * The map of extensions
-	 */
-	private static final Map<Integer, NPCExtension> EXTENSIONS = new HashMap<>();
-	
-	/**
-	 * Registers all extensions
-	 */
-	public static void initialize() {
-		store(13281, new TalkingNPCExtension("Exchange your points into rewards here!", TimeUnit.SECONDS.toMillis(3)));
-		store(2290, new TalkingNPCExtension("Manage your account here!", TimeUnit.SECONDS.toMillis(3)));
-		System.out.println("Initialized " + EXTENSIONS.size() + " npc extensions");
-	}
-	
-	/**
-	 * Stores an extension
-	 */
-	private static void store(int npcId, NPCExtension extension) {
-		EXTENSIONS.put(npcId, extension);
-	}
-	
-	/**
-	 * Gets an extension by the id of the npc
-	 */
-	public static Optional<NPCExtension> getExtension(int npcId) {
-		return Optional.ofNullable(EXTENSIONS.get(npcId));
-	}
-	
+object NPCExtensionHolder {
+    /**
+     * The map of extensions
+     */
+    private val EXTENSIONS: MutableMap<Int, NPCExtension> = HashMap()
+
+    /**
+     * Registers all extensions
+     */
+    fun initialize() {
+        store(13281, TalkingNPCExtension("Exchange your points into rewards here!", TimeUnit.SECONDS.toMillis(3)))
+        store(2290, TalkingNPCExtension("Manage your account here!", TimeUnit.SECONDS.toMillis(3)))
+        logger.info { "Initialized " + EXTENSIONS.size + " npc extensions" }
+    }
+
+    /**
+     * Stores an extension
+     */
+    private fun store(npcId: Int, extension: NPCExtension) {
+        EXTENSIONS[npcId] = extension
+    }
+
+    /**
+     * Gets an extension by the id of the npc
+     */
+    @JvmStatic
+    fun getExtension(npcId: Int): Optional<NPCExtension> {
+        return Optional.ofNullable(EXTENSIONS[npcId])
+    }
+
+    private val logger = InlineLogger()
 }
