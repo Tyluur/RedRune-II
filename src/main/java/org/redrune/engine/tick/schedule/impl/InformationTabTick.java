12590ef10165f32f7971327f1c32407403276068
Tyluur <itstyluur@icloud.com>
2021-04-02 20:22:11 -0400

    update information tab with basic server info

--- src/main/java/org/redrune/engine/tick/schedule/impl/InformationTabTick.java
+++ src/main/java/org/redrune/engine/tick/schedule/impl/InformationTabTick.java
@@ -2,30 +2,40 @@ package org.redrune.engine.tick.schedule.impl;
 
 import org.redrune.engine.tick.schedule.ScheduledTask;
 import org.redrune.game.global.World;
+import org.redrune.utility.constants.ColorConstants;
+import org.redrune.utility.constants.GameConstants;
+import org.redrune.utility.functions.Misc;
 
 /**
  * @author Tyluur
  * @since 2019-05-22
  */
 public class InformationTabTick extends ScheduledTask {
-	
-	/**
-	 * The id of the interface used for this information
-	 */
-	private static final int INTERFACE_ID = 1149;
-	
-	public InformationTabTick() {
-		super(10, -1);
-	}
-	
-	@Override
-	public void run() {
-		World.playerStream().forEach(player -> {
-			//StringBuilder bldr = new StringBuilder();
-			//bldr.append("<col=FF0000>" + GameConstants.SERVER_NAME + " <br><br>");
-			
-			// sends all the text in the stringbuilder
-			//player.getPackets().sendIComponentText(INTERFACE_ID, 16, bldr.toString());
-		});
-	}
+
+    /**
+     * The id of the interface used for this information
+     */
+    private static final int INTERFACE_ID = 930;
+
+    public InformationTabTick() {
+        super(10, -1);
+    }
+
+    @Override
+    public void run() {
+        World.playerStream().forEach(player -> {
+            StringBuilder bldr = new StringBuilder();
+
+            bldr.append("<col=FF0000>" + GameConstants.SERVER_NAME + " <br><br>");
+            bldr.append("Online: <col=" + ColorConstants.WHITE + ">").append(World.getPlayers().size()).append("<br>");
+
+
+            bldr.append("<br><col=FF0000>Player<br><br>");
+
+            bldr.append("Rank: <col=" + ColorConstants.WHITE + ">").append(Misc.formatPlayerNameForDisplay(player.getDominantRight().getFormattedName())).append("<br>");
+
+            // sends all the text in the stringbuilder
+            player.getPackets().sendIComponentText(INTERFACE_ID, 16, bldr.toString());
+        });
+    }
 }
--- src/main/java/org/redrune/game/entity/actor/player/link/InterfaceManager.java
+++ src/main/java/org/redrune/game/entity/actor/player/link/InterfaceManager.java
@@ -262,7 +262,7 @@ public class InterfaceManager {
 		sendTab(resizableScreen ? 91 : 205, interfaceId);
 		player.getPackets().sendIComponentText(interfaceId, 10, "<col=" + ColorConstants.RED + ">Information");
 		player.getPackets().sendIComponentText(interfaceId, 16, "");
-//		player.getPackets().sendHideIComponent(930, 12, true); //scroll bar
+		player.getPackets().sendHideIComponent(930, 12, true); //scroll bar
 		for (byte i = 17; i < 25; i++) {
 			player.getPackets().sendHideIComponent(930, i, true);
 		}
