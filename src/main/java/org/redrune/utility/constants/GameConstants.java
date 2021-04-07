75ec2290ece40ea1559d9e09f906986125fa1d33
Tyluur <itstyluur@icloud.com>
2021-04-07 19:30:08 -0400

    change respawn tile

--- src/main/java/org/redrune/utility/constants/GameConstants.java
+++ src/main/java/org/redrune/utility/constants/GameConstants.java
@@ -25,7 +25,7 @@ public interface GameConstants {
 	/**
 	 * The location that players who die spawn at
 	 */
-	WorldTile RESPAWN_PLAYER_LOCATION = new WorldTile(3163, 3497, 0);
+	WorldTile RESPAWN_PLAYER_LOCATION = new WorldTile(3164, 3483, 0);
 	
 	/**
 	 * If we're hosted on linux
