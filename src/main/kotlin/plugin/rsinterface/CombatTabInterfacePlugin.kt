e6c0ad99497f89ea5ff1ab5d002aed381c075da1
Tyluur <itstyluur@icloud.com>
2021-04-07 19:41:43 -0400

    remove spec toggle delay

--- src/main/kotlin/plugin/rsinterface/CombatTabInterfacePlugin.kt
+++ src/main/kotlin/plugin/rsinterface/CombatTabInterfacePlugin.kt
@@ -1,10 +1,8 @@
 package plugin.rsinterface
 
-import org.redrune.engine.SystemManager
 import org.redrune.game.content.entity.actor.combat.CombatAlgorithm
 import org.redrune.game.content.plugin.type.InterfacePlugin
 import org.redrune.game.entity.actor.player.Player
-import java.util.concurrent.TimeUnit
 
 /**
  * @author Tyluur <itstyluur></itstyluur>@icloud.com>
@@ -21,16 +19,7 @@ class CombatTabInterfacePlugin : InterfacePlugin {
     ): Boolean {
         if (componentId == 4) {
             player.putTemporaryAttribute("special_attack_toggled", true)
-            SystemManager.SLOW_EXECUTOR.schedule({
-                try {
-                    if (player.isDead) {
-                        return@schedule
-                    }
-                    CombatAlgorithm.checkSpecialToggle(player, 0)
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                }
-            }, 400, TimeUnit.MILLISECONDS)
+            CombatAlgorithm.checkSpecialToggle(player, 0)
         } else if (componentId >= 11 && componentId <= 14) {
             player.combatDefinitions.attackStyle = componentId - 11
         } else if (componentId == 15) {
