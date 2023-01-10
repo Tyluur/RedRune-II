9fc9209dab53450a8b49b82dea85fc2a095cf7f9
Tyluur <itstyluur@icloud.com>
2023-01-09 22:55:38 -0500

    feat: convert JS5ThreadFactory to kotlin

--- src/main/java/org/redrune/engine/tick/schedule/impl/PunishmentProcessorTick.kt
+++ src/main/java/org/redrune/engine/tick/schedule/impl/PunishmentProcessorTick.kt
@@ -10,7 +10,7 @@ import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 
 /**
- * @author Tyluur <itstyluur></itstyluur>@icloud.com>
+ * @author Tyluur <itstyluur@icloud.com>
  * @since 9/13/2017
  */
 class PunishmentProcessorTick : ScheduledTask(5, -1) {
