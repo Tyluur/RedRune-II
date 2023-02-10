c97cbe940d1d61ff361bd5b07204938d879039fa
Tyluur <itstyluur@icloud.com>
2023-02-09 20:54:15 -0500

    chore: refactor PlayersOnlineCommandPlugin

--- src/main/java/plugin/command/player/PlayersOnlineCommandPlugin.kt
+++ src/main/java/plugin/command/player/PlayersOnlineCommandPlugin.kt
@@ -5,18 +5,18 @@ import org.redrune.game.entity.actor.player.Player
 import org.redrune.game.global.World
 import org.redrune.utility.constants.InterfaceConstants
 import plugin.command.CommandManifest
-import java.util.*
 
 /**
- * @author Tyluur <itstyluur></itstyluur>@icloud.com>
+ * @author Tyluur <itstyluur@icloud.com>
  * @since 9/1/2017
  */
 @CommandManifest(description = "Shows the players online")
 class PlayersOnlineCommandPlugin : CommandPlugin() {
     override fun handle(player: Player, args: Array<String>, console: Boolean, clientCommand: Boolean) {
-        val messages: MutableList<String> = ArrayList()
-        World.playerStream()
-            .forEach { p: Player -> messages.add("" + p.displayName + " (lvl. " + p.skills.combatLevel + ")") }
+        val messages = ArrayList<String>()
+
+        World.playerStream().forEach { p: Player -> messages.add("${p.displayName} (lvl. " + p.skills.combatLevel + ")") }
+
         sendResponse(player, "There are currently " + World.getPlayers().size + " players online.", console)
 
         InterfaceConstants.sendQuestScroll(player, "Dusk", *messages.toTypedArray())
