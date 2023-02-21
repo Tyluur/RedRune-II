6b0b3359dd40687fb5d02df2c99616cedd576224
Tyluur <itstyluur@icloud.com>
2023-02-20 22:14:50 -0500

    feat: add more climbable objects

--- src/main/java/plugin/command/owner/ReloadDoorsCommandPlugin.kt
+++ src/main/java/plugin/command/owner/ReloadDoorsCommandPlugin.kt
@@ -1,16 +1,17 @@
 package plugin.command.owner
 
 import org.redrune.game.content.plugin.type.CommandPlugin
-import plugin.command.CommandManifest
 import org.redrune.game.entity.actor.player.Player
 import org.redrune.utility.game.repository.`object`.door.DoorRepository
+import plugin.command.CommandManifest
 
 /**
- * @author Tyluur <itstyluur></itstyluur>@icloud.com>
+ * @author Tyluur <itstyluur@icloud.com>
  * @since 2019-02-20
  */
 @CommandManifest(description = "Reloads all doors from file")
 class ReloadDoorsCommandPlugin : CommandPlugin() {
+
     override fun handle(player: Player, args: Array<String>, console: Boolean, clientCommand: Boolean) {
         DoorRepository.initialize()
         player.packets.sendMessage("Reloaded all game doors")
@@ -19,4 +20,5 @@ class ReloadDoorsCommandPlugin : CommandPlugin() {
     override fun identifiers(): Array<String> {
         return arguments("rldrs", "rldoors")
     }
+
 }
