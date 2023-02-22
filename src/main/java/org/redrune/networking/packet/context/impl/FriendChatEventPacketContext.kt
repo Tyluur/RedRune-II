5899bbad7585786b6b05e9b537ea1928057efdad
Tyluur <itstyluur@icloud.com>
2023-02-22 11:18:00 -0500

    chore: fix author tags throughout the source with bad html formatting

--- src/main/java/org/redrune/networking/packet/context/impl/FriendChatEventPacketContext.kt
+++ src/main/java/org/redrune/networking/packet/context/impl/FriendChatEventPacketContext.kt
@@ -4,7 +4,7 @@ import org.redrune.game.entity.actor.player.Player
 import org.redrune.networking.packet.context.PacketContext
 
 /**
- * @author Tyluur <itstyluur>@icloud.com>
+ * @author Tyluur <itstyluur@icloud.com>
  * @since 2019-02-04
  */
 class FriendChatEventPacketContext : PacketContext() {
