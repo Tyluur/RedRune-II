633fa45d5945bacf0995f6844d8becb2964a2a64
Tyluur <itstyluur@icloud.com>
2023-02-24 13:45:22 -0500

    chore: convert JsonFileManager

--- src/main/java/org/redrune/utility/file/JsonFileManager.kt
+++ src/main/java/org/redrune/utility/file/JsonFileManager.kt
@@ -1,38 +1,36 @@
-package org.redrune.utility.file;
+package org.redrune.utility.file
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-import org.redrune.utility.functions.Misc;
-
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-import java.lang.reflect.Modifier;
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import org.redrune.utility.functions.Misc
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.lang.reflect.Modifier
 
 /**
  * @author Tyluur <itstyluur@icloud.com>
  * @since 8/30/2017
  */
-public class JsonFileManager {
+object JsonFileManager {
 
     /**
      * The gson instance
      */
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private val GSON = GsonBuilder().setPrettyPrinting().create()
 
     /**
      * Loads the file data
      *
      * @param file The file to load data from
      */
-    public static <K> K loadJsonData(File file) {
-        if (!file.exists()) {
-            return null;
-        }
-        return GSON.fromJson(Misc.getText(file.getAbsolutePath()), new TypeToken<K>() {
-        }.getType());
+    fun <K> loadJsonData(file: File): K? {
+        return if (!file.exists()) {
+            null
+        } else GSON.fromJson(
+            Misc.getText(file.absolutePath),
+            object : TypeToken<K>() {}.type
+        )
     }
 
     /**
@@ -41,17 +39,19 @@ public class JsonFileManager {
      * @param data     The list to save
      * @param location The location to save to
      */
-    @SuppressWarnings("hiding")
-    public static <T> boolean save(T data, String location) {
-        try (Writer writer = new FileWriter(location)) {
-            GsonBuilder builder = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().excludeFieldsWithModifiers(Modifier.TRANSIENT, Modifier.STATIC);
-            Gson gson = builder.create();
-            gson.toJson(data, writer);
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return false;
+    fun <T> save(data: T, location: String?): Boolean {
+        try {
+            FileWriter(location).use { writer ->
+                val builder = GsonBuilder().setPrettyPrinting().disableHtmlEscaping().excludeFieldsWithModifiers(
+                    Modifier.TRANSIENT, Modifier.STATIC
+                )
+                val gson = builder.create()
+                gson.toJson(data, writer)
+                return true
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return false
         }
     }
-
 }
