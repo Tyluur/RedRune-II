c2c5958dc25813da8d3ef4592f79c5a0efff0e4d
Tyluur <itstyluur@icloud.com>
2023-02-25 14:36:12 -0500

    fix: convert outgoing packet model to kotlin

--- src/main/java/org/redrune/networking/packet/outgoing/OutgoingPacketBuilder.kt
+++ src/main/java/org/redrune/networking/packet/outgoing/OutgoingPacketBuilder.kt
@@ -1,33 +1,26 @@
-package org.redrune.networking.packet.outgoing;
+package org.redrune.networking.packet.outgoing
 
-
-import org.redrune.networking.packet.Packet;
-import org.redrune.networking.packet.PacketBuilder;
+import org.redrune.networking.packet.Packet
+import org.redrune.networking.packet.PacketBuilder
 
 /**
+ * Constructs the outgoing packet bldr
  * @author Tyluur <itstyluur@icloud.com>
  * @since 2019-02-02
  */
-public abstract class OutgoingPacketBuilder {
+abstract class OutgoingPacketBuilder
+    (
+        /**
+         * The packet builder instance
+         */
+        @JvmField protected val bldr: PacketBuilder,
+) {
 
     /**
-     * The packet builder instance
-     */
-
-    protected final PacketBuilder bldr;
-
-    /**
-     * Constructs the outgoing packet bldr
-     */
-    public OutgoingPacketBuilder(PacketBuilder packetBuilder) {
-        this.bldr = packetBuilder;
-    }
-
-    /**
-     * The building of the packet is handled in this method. The {@code PacketBuilder} is converted to a {@code Packet}
-     * via {@link PacketBuilder#toPacket()}
+     * The building of the packet is handled in this method. The `PacketBuilder` is converted to a `Packet`
+     * via [PacketBuilder.toPacket]
      *
      * @return A newly constructed packet
      */
-    public abstract Packet build();
+    abstract fun build(): Packet
 }
