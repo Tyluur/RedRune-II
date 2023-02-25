3ea322554739aeddc437bb22d16e38686e8193aa
Tyluur <itstyluur@icloud.com>
2023-02-25 14:11:06 -0500

    feat: convert cache scripts to kotlin

--- src/main/java/com/alex/scripts/IndexPacker.kt
+++ src/main/java/com/alex/scripts/IndexPacker.kt
@@ -1,19 +1,19 @@
-package com.alex.scripts;
+package com.alex.scripts
 
-import com.alex.store.Store;
-
-import java.io.IOException;
+import com.alex.store.Store
+import java.io.IOException
 
 /**
  * @author Tyluur <itstyluur@icloud.com>
  * @since 5/15/2016
  */
-public class IndexPacker {
+object IndexPacker {
 
-    public static void main(String[] args) throws IOException {
-        Store toPackTo = new Store("C:\\Users\\Tyler\\Desktop\\cache\\", false);
-        Store toPackFrom = new Store("E:\\3.5.16 - Muth backup\\Me\\Programming\\- SERVERS -\\600+\\Alotic\\update_server\\cache\\", false);
-
+    @Throws(IOException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val toPackTo = Store("C:\\Users\\Tyler\\Desktop\\cache\\", false)
+        val toPackFrom =
+            Store("E:\\3.5.16 - Muth backup\\Me\\Programming\\- SERVERS -\\600+\\Alotic\\update_server\\cache\\", false)
     }
-
 }
--- src/main/java/com/alex/scripts/MapReplacer.kt
+++ src/main/java/com/alex/scripts/MapReplacer.kt
@@ -1,21 +1,22 @@
-package com.alex.scripts;
+package com.alex.scripts
 
-import com.alex.store.Store;
-
-import java.io.IOException;
+import com.alex.store.Store
+import java.io.IOException
 
 /**
  * @author Tyluur <itstyluur@icloud.com>
  * @since 1/4/2017
  */
-public class MapReplacer {
+object MapReplacer {
 
-    public static void main(String[] args) throws IOException {
+    @Throws(IOException::class)
+    @JvmStatic
+    fun main(args: Array<String>) {
         // this is where the map should be placed to
-        Store replaced = new Store("./data/essentials/cache/"); // 667 cache
-        Store origonal = new Store("C:\\Users\\Tyler\\Downloads\\Dementhium 637\\Dementhium 637\\data\\cache\\"); // 639 cache
-		
-		/*int count = 1, regionId = 12598;
+        val replaced = Store("./data/essentials/cache/") // 667 cache
+        val origonal = Store("C:\\Users\\Tyler\\Downloads\\Dementhium 637\\Dementhium 637\\data\\cache\\") // 639 cache
+
+        /*int count = 1, regionId = 12598;
 		
 		int regionX = (regionId >> 8) * 64;
 		int regionY = (regionId & 0xff) * 64;
@@ -38,18 +39,15 @@ public class MapReplacer {
 		// 667 keys
 		replaced.getIndexes()[5].putFile(orgArchive, 0, Constants.GZIP_COMPRESSION, data, new int[] { 1251254967, -652948165, 1911744550, -595699947 }, false, false, Utils.getNameHash(replacedName), -1);
 		replaced.getIndexes()[5].rewriteTable(); // Now we are done, test it out.*/
-
-        replaced.getIndexes()[2].putArchive(2, origonal);
-        replaced.getIndexes()[2].putArchive(4, origonal);
-        replaced.getIndexes()[5].packIndex(5, origonal, false);
-
-        replaced.getIndexes()[7].packIndex(origonal);
-        replaced.getIndexes()[18].packIndex(origonal);
-
-        replaced.getIndexes()[18].putArchive(18, origonal);
-        replaced.getIndexes()[7].putArchive(7, origonal);
+        replaced.indexes[2].putArchive(2, origonal)
+        replaced.indexes[2].putArchive(4, origonal)
+        replaced.indexes[5].packIndex(5, origonal, false)
+        replaced.indexes[7].packIndex(origonal)
+        replaced.indexes[18].packIndex(origonal)
+        replaced.indexes[18].putArchive(18, origonal)
+        replaced.indexes[7].putArchive(7, origonal)
 
         //replaced.getIndexes()[2].packIndex(2, origonal, false);
-        System.out.println("Completed successfully.");
+        println("Completed successfully.")
     }
 }
